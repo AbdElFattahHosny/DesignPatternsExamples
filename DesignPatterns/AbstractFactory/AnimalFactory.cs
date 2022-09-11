2c94455e26b99bb09da930826c4fbf09222ed379
abdel <abdel@DESKTOP-1CBJH64>
2022-09-11 07:12:14 +0200

    Add project files.

---
+++ DesignPatterns/AbstractFactory/AnimalFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AbstractFactory
+{
+    public abstract class AnimalFactory
+    {
+        //to use it in subFactories to get object of each animal kind class
+        public abstract Animal GetAnimal(string AnimalType);
+        public static AnimalFactory CreateAnimalFactory(string FactoryType)
+        {
+            if (FactoryType.Equals("Sea"))
+                return new SeaAnimalFactory();
+            else
+                return new LandAnimalFactory();
+        }
+    }
+}
---
+++ DesignPatterns/AbstractFactory/Shark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AbstractFactory
+{
+    public class Shark : Animal
+    {
+        public string speak()
+        {
+            return "Bla bla bla";
+        }
+    }
+}
